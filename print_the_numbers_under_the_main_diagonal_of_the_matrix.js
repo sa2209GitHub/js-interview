aa2dd6fb149ea1aa12472ac9572642d3a6397faa
Sergey Arkhangelskiy <sergey.arkhangelskiy@gmail.com>
2022-10-12 16:01:21 +0300

    Added task: reverse array without creating another array

--- print_the_numbers_under_the_main_diagonal_of_the_matrix.js
+++ print_the_numbers_under_the_main_diagonal_of_the_matrix.js
@@ -1,8 +1,8 @@
 /**
- *  Task:
- *      print the numbers
- *      under the main diagonal
- *      of the matrix
+ *      Task
+ *  Print the numbers
+ *  under the main diagonal
+ *  of the matrix
  */
 
 const matrix = [
@@ -10,10 +10,10 @@ const matrix = [
     [15, 16, 17, 18],
     [19, 20, 21, 22],
     [23, 24, 25, 26]
-]
+];
 
 for (let i = 1; i < matrix.length; i++) {
     for (let j = 0; j < i; j++) {
         console.log(matrix[i][j]);
-    }
-}
+    };
+};
