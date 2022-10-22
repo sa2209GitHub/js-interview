9443f4a806bf1a52339b31455e58cb7e8ebd108b
Sergey Arkhangelskiy <sergey.arkhangelskiy@gmail.com>
2022-10-22 17:29:22 +0300

    Added task: how to make an integer from float

---
+++ tasks/how_to_check_if_number_is_integer.js
@@ -0,0 +1,10 @@
+/**
+ *  Task:
+ *      How to check
+ *      if Number is Integer
+ */
+
+const n = 42;
+
+// 1. Number.isInteger()
+console.log(`Number.isInteger(${n}) =`, Number.isInteger(n));
--- tasks/how_to_make_an_integer_from_float.js
+++ tasks/how_to_make_an_integer_from_float.js
@@ -7,29 +7,21 @@
 const floatNumber = 123.456789;
 
 // 1. ES6 Way
-console.log('1. ES6 Way', floatNumber);
-console.log('   Math.trunc(floatNumber) =', Math.trunc(floatNumber), '\n');
+console.log(`Math.trunc(${floatNumber}) =`, Math.trunc(floatNumber));
 
 // 2. Round
-console.log('2. Round', floatNumber);
-console.log('   Math.round(floatNumber) =', Math.round(floatNumber));
-console.log('   Math.ceil(floatNumber) =', Math.ceil(floatNumber));
-console.log('   Math.floor(floatNumber) =', Math.floor(floatNumber), '\n');
-
-// 3. parseInt()
-console.log('3. parseInt()', floatNumber);
-console.log('   parseInt(floatNumber, 10) =', parseInt(floatNumber, 10), '\n');
+console.log(`Math.round(${floatNumber}) =`, Math.round(floatNumber));
+console.log(`Math.ceil(${floatNumber}) =`, Math.ceil(floatNumber));
+console.log(`Math.floor(${floatNumber}) =`, Math.floor(floatNumber));
+console.log(`parseInt(${floatNumber}) =`, parseInt(floatNumber, 10), '\n');
 
 // 4. Int32Array
-console.log(`4. Int32Array([${floatNumber}])`);
 const newInt32Array = new Int32Array([floatNumber]);
-console.log('   const newInt32Array = new Int32Array([floatNumber])', '\n   newInt32Array[0] =', newInt32Array[0], '\n');
+console.log(`const newInt32Array = new Int32Array(${floatNumber})\n      newInt32Array[0] =`, newInt32Array[0], '\n');
 
 // 5. Bitwise OR
-console.log('5. Bitwise OR', floatNumber);
-console.log('   floatNumber | 0 = ', floatNumber | 0, '\n');
+console.log(`${floatNumber} | 0 = `, floatNumber | 0);
 
 // 5. Bitwise NOT
-console.log('6. Bitwise NOT', floatNumber);
-console.log('   ~~ floatNumber = ', ~~ floatNumber);
-console.log('   ~~ 2.0 =', ~~ 2.0, '\n');
+console.log(`~~ ${floatNumber} = `, ~~ floatNumber);
+console.log(`~~ 2.0 =`, ~~ 2.0);
