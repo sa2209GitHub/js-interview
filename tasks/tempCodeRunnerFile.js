155cba563fe5c9166450255a83d062a9389e30c1
Sergey Arkhangelskiy <sergey.arkhangelskiy@gmail.com>
2022-11-18 03:45:47 +0300

    Rotate a Matrix Clockwise

--- tasks/tempCodeRunnerFile.js
+++ tasks/tempCodeRunnerFile.js
@@ -1,15 +1,11 @@
-kwise(matrix1));
-console.table(rotateMatrixClockwise(matrix2));
-console.table(rotateMatrixClockwise(matrix3));
+(matrix1));
+// console.table(rotateMatrixClockwise(matrix2));
+// console.table(rotateMatrixClockwise(matrix3));
 
-console.table(rotateMatrixClockwiseSecond(matrix1));
-console.table(rotateMatrixClockwiseSecond(matrix2));
-console.table(rotateMatrixClockwiseSecond(matrix3));
+// console.table(rotateMatrixClockwiseSecond(matrix1));
+// console.table(rotateMatrixClockwiseSecond(matrix2));
+// console.table(rotateMatrixClockwiseSecond(matrix3));
 
-console.table(rotateMatrixClockwiseThird(matrix1));
-console.table(rotateMatrixClockwiseThird(matrix2));
-console.table(rotateMatrixClockwiseThird(matrix3));
-
-console.table(rotateMatrixClockwiseFourth(matrix1));
-console.table(rotateMatrixClockwiseFourth(matrix2));
-console.table(rotateMatrixClockwiseFourth(matrix3));
+// console.table(rotateMatrixClockwiseThird(matrix1));
+// console.table(rotateMatrixClockwiseThird(matrix2));
+// console.table(rotateMatrixClockwiseThird(matrix3));
